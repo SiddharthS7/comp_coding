e7d3633121fec15bdf357b53f77122646d6531cf
Siddharth Kumar Shakya <32535419+SiddharthS7@users.noreply.github.com>
2022-08-21 15:51:09 +0530

    Time: 4 ms (82.55%), Space: 44.7 MB (17.55%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -3,18 +3,15 @@ class Solution {
         HashMap<Integer,Integer> hm=new HashMap<>();
         int[] ans = new int[nums1.length];
         for(int i=0;i<nums2.length;i++)hm.put(nums2[i],i);
-        System.out.println(hm);
         for(int i=0;i<nums1.length;i++) {
             int k=hm.get(nums1[i]);
             for(int j=k+1;j<nums2.length;j++) {
                 if(nums2[j] > nums1[i]) {
                     ans[i]=nums2[j];
                     break;
-                }else{
-                    ans[i]=-1;
                 }
             }
-            if(k==nums2.length-1)ans[i]=-1;
+            if(ans[i]==0)ans[i]=-1;
         }
         return ans;
     }
