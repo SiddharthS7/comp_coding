14fdc73674530b2ed542d3d1b7eb8f77eac04abc
Siddharth Kumar Shakya <32535419+SiddharthS7@users.noreply.github.com>
2022-08-21 15:50:03 +0530

    Time: 11 ms (10.51%), Space: 45.3 MB (5.85%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans=new int[nums1.length];
         HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<nums2.length;i++) {
-            for(int j=i+1;j<nums2.length;j++) {
-                if(nums2[j] > nums2[i]) {
-                    hm.put(nums2[i],nums2[j]);
-                    break;
-                }else{
-                    hm.put(nums2[i],-1);
-                }
-            }
-        }
-        hm.put(nums2[nums2.length-1],-1);
+        int[] ans = new int[nums1.length];
+        for(int i=0;i<nums2.length;i++)hm.put(nums2[i],i);
         System.out.println(hm);
         for(int i=0;i<nums1.length;i++) {
-            ans[i] = hm.get(nums1[i]);
+            int k=hm.get(nums1[i]);
+            for(int j=k+1;j<nums2.length;j++) {
+                if(nums2[j] > nums1[i]) {
+                    ans[i]=nums2[j];
+                    break;
+                }else{
+                    ans[i]=-1;
+                }
+            }
+            if(k==nums2.length-1)ans[i]=-1;
         }
         return ans;
     }
