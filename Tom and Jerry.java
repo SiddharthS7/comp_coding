4fe20366c08c136da53b3c7bd995336067b03d86
Siddharth Kumar Shakya <32535419+SiddharthS7@users.noreply.github.com>
2022-09-26 23:04:32 +0530

    Create Tom and Jerry.java

---
+++ Tom and Jerry.java
@@ -0,0 +1,8 @@
+//https://practice.geeksforgeeks.org/problems/tom-and-jerry1325/1
+
+class Solution {
+    static int numsGame(int N) {
+        // code here
+        return (N%2==0)?1:0;
+    }
+};
