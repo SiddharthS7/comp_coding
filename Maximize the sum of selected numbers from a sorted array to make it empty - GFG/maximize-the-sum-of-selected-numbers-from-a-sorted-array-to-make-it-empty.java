89b0c5c71a6ab2c4d5439b85219622e9c908ae39
Siddharth Kumar Shakya <32535419+SiddharthS7@users.noreply.github.com>
2022-10-14 23:57:28 +0530

    Added solution - LeetHub

---
+++ Maximize the sum of selected numbers from a sorted array to make it empty - GFG/maximize-the-sum-of-selected-numbers-from-a-sorted-array-to-make-it-empty.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		     
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    Arrays.sort(arr);
+		    Complete obj = new Complete();
+		    int ans = obj.maximizeSum(arr, sizeOfArray);
+		    System.out.println(ans);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int maximizeSum (int arr[], int n) {
+        //Complete the function
+        int[] m=new int [100000];
+        Arrays.fill(m,0);
+        for(int i:arr) {
+            m[i]++;
+        }
+        int ans=0;
+        for(int i=n-1;i>=0;i--) {
+            int cur=arr[i];
+            if(m[cur]!=0){
+                m[cur]--;
+                if(m[cur-1]!=0)m[cur-1]--;
+                ans+=cur;
+            }
+        }
+        return ans;
+    }
+    
+    
+}
+
+
